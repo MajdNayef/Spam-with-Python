7a02d9e51c515afccd42fa021fa4954f32db017d
MajdNayef <105067273+MajdNayef@users.noreply.github.com>
2022-08-08 05:10:25 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 time.sleep(2)
-f = open ("path",'r') #add the path to the script file here. script file is a txt file  
+f = open ("instagram",'r') #add the path to the script file here. script file is a txt file  
 d = open ("script",'r')
 for word in d:
     pyautogui.typewrite(word)
